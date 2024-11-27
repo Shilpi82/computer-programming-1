d81c4d50200a9ce522ceaa0612262041373877a0
Shilpi82 <twrshilpi82@gmail.com>
2024-11-27 23:26:37 +0530

    Ques_05

---
+++ coding-assignment/function/function_q05.py
@@ -0,0 +1,5 @@
+#Function that accepts a list of numbers and returns the maximum and minimum numbers from the list
+def find_max_min(nums):
+    return max(nums), min(nums)
+
+print(find_max_min([1, 2, 3, 4, 5]))  # Output: (5, 1)
