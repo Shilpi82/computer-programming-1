b82535e0768dd6ec091cdd3ea50a4f5da5b543d7
Shilpi82 <twrshilpi82@gmail.com>
2024-11-28 00:23:27 +0530

    Ques_10

---
+++ coding-assignment/tuple/tuple_q10.py
@@ -0,0 +1,5 @@
+#Function that returns the tuple with the even-indexed elements
+def even_index_elements(tup):
+    return tup[::2]
+
+print(even_index_elements((1, 2, 3, 4, 5, 6)))  # Output: (1, 3, 5)
