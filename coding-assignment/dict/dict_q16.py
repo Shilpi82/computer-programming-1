935e93db5668efebf045d2379bda2918faa2e99d
Shilpi82 <twrshilpi82@gmail.com>
2024-11-27 23:08:40 +0530

    Ques_16

---
+++ coding-assignment/dict/dict_q16.py
@@ -0,0 +1,5 @@
+# Get the value of a key if it exists, or a default value if it doesn't.
+my_dict = {"a": 1, "b": 2}
+print(my_dict.get("a", 0))  # Output: 1
+print(my_dict.get("c", 0))  # Output: 0
+
