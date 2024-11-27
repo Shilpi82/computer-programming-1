c656b3fb3b4c8b0f773bf7cb09f667dc79cc084d
Shilpi82 <twrshilpi82@gmail.com>
2024-11-28 00:28:12 +0530

    Que.1

---
+++ coding-assignment/string/string_q01.py
@@ -0,0 +1,9 @@
+# Reverses a string without using Python's
+def reverse_string(s):
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+print(reverse_string("hello"))  # Output: "olleh"
+
