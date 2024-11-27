2c313ef3eb5b1bc76d352ed2868abe3a214c51cc
Shilpi82 <twrshilpi82@gmail.com>
2024-11-28 00:29:54 +0530

    Que.03

---
+++ coding-assignment/string/string_q03.py
@@ -0,0 +1,6 @@
+# Count the occurrences of a substring within a string
+def count_substring(s, substring):
+    return s.count(substring)
+
+print(count_substring("hellohellohello", "hello"))  # Output: 3
+
