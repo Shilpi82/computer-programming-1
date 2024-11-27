1cbbd8e4c6945a3d87e9d007eb552feb774483c9
Shilpi82 <twrshilpi82@gmail.com>
2024-11-28 00:31:00 +0530

    Que.05

---
+++ coding-assignment/string/string_04.py
@@ -0,0 +1,6 @@
+#Function that removes all vowels from a string
+def remove_vowels(s):
+    vowels = "aeiouAEIOU"
+    return ''.join([char for char in s if char not in vowels])
+
+print(remove_vowels("hello world"))  # Output: "hll wrld"
