19901e1e48d05c030c1ec84fa4fe9b64f82876bb
Shilpi82 <twrshilpi82@gmail.com>
2024-11-28 00:33:43 +0530

    Que.06

---
+++ coding-assignment/string/string_q06.py
@@ -0,0 +1,9 @@
+#Find the longest substring without repeating characters
+def longest_substring(s):
+    char_set = set()
+    left = 0
+    max_len = 0
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 
