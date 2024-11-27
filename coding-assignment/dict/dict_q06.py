8f53e1952bdaa4b7cb3d3c94e49ec7610a4202aa
Shilpi82 <twrshilpi82@gmail.com>
2024-11-27 22:52:47 +0530

    Ques_06

---
+++ coding-assignment/dict/dict_q06.py
@@ -0,0 +1,4 @@
+# Create a dictionary where the keys are a list of numbers and the values are their squares
+numbers = [1, 2, 3, 4, 5]
+squares_dict = {num: num ** 2 for num in numbers}
+print(squares_dict)  # Output: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
