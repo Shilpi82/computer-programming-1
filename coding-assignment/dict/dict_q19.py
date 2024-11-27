48349f7869e86328a86cd8d9adf52ca2a9da6293
Shilpi82 <twrshilpi82@gmail.com>
2024-11-27 23:12:26 +0530

    Ques_19

---
+++ coding-assignment/dict/dict_q19.py
@@ -0,0 +1,6 @@
+# Create a dictionary from two lists (one for keys and one for values)
+keys = ["name", "age", "gender"]
+values = ["Alice", 25, "Female"]
+my_dict = dict(zip(keys, values))
+print(my_dict)  # Output: {"name": "Alice", "age": 25, "gender": "Female"}
+
