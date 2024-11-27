f285fbe178914f9f09d277c54dcc801dc3ad9e0a
Shilpi82 <twrshilpi82@gmail.com>
2024-11-28 00:20:52 +0530

    Ques_07

---
+++ coding-assignment/tuple/tuple_q07.py
@@ -0,0 +1,6 @@
+#Function that returns the tuple with elements from index 2 to 5
+def slice_tuple(tup):
+    return tup[2:6]
+
+print(slice_tuple((10, 20, 30, 40, 50, 60, 70)))  # Output: (30, 40, 50, 60)
+
