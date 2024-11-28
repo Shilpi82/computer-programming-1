383a42f1be4437e015d9e592f1c871c2c62d7c94
Shilpi82 <twrshilpi82@gmail.com>
2024-11-28 13:52:14 +0530

    Ques_05

---
+++ coding-assignment/list/list_q05.py
@@ -0,0 +1,7 @@
+#The Sublist with the Maximum Sum
+def maxSubArray(nums: list) -> int:
+    max_sum = current_sum = nums[0]
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+    return max_sum
